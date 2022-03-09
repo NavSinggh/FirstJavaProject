42d24f1b7b672e0ad5dc965c6e13d3e4a740d16d
NAV'S DELL <NAV'S DELL@DESKTOP-10BC6C9>
2022-03-09 19:52:44 +0530

    First commit

---
+++ Unit_01/src/First/main.java
@@ -0,0 +1,15 @@
+package First;
+
+public class main {
+
+	public static void main(String[] args) {
+		String text ="Naveen Singh";
+		System.out.println(text);
+		text = text.toUpperCase();
+		System.out.println(text.length());
+		System.out.println(text.isEmpty());
+		System.out.println(text.indexOf('A'));
+	
+	}
+
+}
